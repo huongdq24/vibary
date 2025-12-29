446e298bd639407303badba0618195bac4d52b37
Cong Nguyen <congnguyen.vesper007@gmail.com>
2025-12-29 03:41:55 +0000

    https://images.prismic.io/lf-web/ZqcFKB5LeNNTxjJn_SecretGarden2.png?auto

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.prismic.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
