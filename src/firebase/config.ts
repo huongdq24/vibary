186d3d2f7dff6677b0c3fc473d2413d6cdd5ed3d
Cong Nguyen <congnguyen.vesper007@gmail.com>
2025-12-30 03:31:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "gen-lang-client-0850828234",
+  "appId": "1:870023233768:web:111bbee0df62cf0519c95d",
+  "apiKey": "AIzaSyCmkjxRytvOo4zDL2vJZjjjQ1avteL1idM",
+  "authDomain": "gen-lang-client-0850828234.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "870023233768"
+};
