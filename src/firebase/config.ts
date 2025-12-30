703762f8e4d0f2504e818d6f44824bfce2d5a78a
Cong Nguyen <congnguyen.vesper007@gmail.com>
2025-12-30 03:57:01 +0000

    fix errors ## Error Type Console FirebaseError
    ## Error Message
    Firebas

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "gen-lang-client-0850828234",
   "appId": "1:870023233768:web:111bbee0df62cf0519c95d",
+  "storageBucket": "gen-lang-client-0850828234.appspot.com",
   "apiKey": "AIzaSyCmkjxRytvOo4zDL2vJZjjjQ1avteL1idM",
   "authDomain": "gen-lang-client-0850828234.firebaseapp.com",
   "measurementId": "",
